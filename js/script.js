4462e518b17cf44675b4dd3f861ea62c04c75752
Soham Verma <sohamverma26@gmail.com>
2023-02-27 13:58:39 +1100

    Add files via upload

--- js/script.js
+++ js/script.js
@@ -86,36 +86,3 @@ video.onended = function () {
   ppbtn.setAttribute('title', 'Play');
 }
 // .video player
-
-// cookies
-// check user settings
-function GetCookie(name) {
-  var arg=name+"=";
-  var alen=arg.length;
-  var clen=document.cookie.length;
-  var i=0;
-  while (i<clen) {
-	var j=i+alen;
-	if (document.cookie.substring(i,j)==arg)
-	  return "here";
-	i=document.cookie.indexOf(" ",i)+1;
-	if (i==0) break;
-  }
-  return null;
-}
-// testing cookie
-function testFirstCookie(){
-	var visit=GetCookie("FirstTimeVisitCookie");
-	if (visit==null){
-	   var expire=new Date();
-	   expire=new Date(expire.getTime()+7776000000);
-	   document.cookie="FirstTimeVisitCookie=here; expires="+expire + ";path=/";
-	   console.log("Welcome! You are here for the first time!");	// New visitor
-   } else {
-	   console.log("Welcome back!");	// Returning visitor
-   }
-}
-$(document).ready(function(){
-	testFirstCookie();
-});
-// .cookies
